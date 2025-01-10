8e9f213664c4084a71b4d5d2c42db1c982d83fe9
Gleb <berezin@fgosniias.ru>
2025-01-10 14:12:59 +0300

    keymap

--- init.lua
+++ init.lua
@@ -9,6 +9,11 @@ vim.opt.shiftwidth = 4
 vim.opt.autoindent = false
 vim.opt.smartindent = false
 
+vim.opt.fileformat = "unix" -- lf
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -26,10 +31,12 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }, -- theme
+    { "Wansmer/langmapper.nvim", name="langmapper", priority = 1}, -- normal mode in ru keymap
 }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
 require("catppuccin").setup()
+require("langmapper").setup()
 vim.cmd.colorscheme "catppuccin"
