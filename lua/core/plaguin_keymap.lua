d32089bc43de17c849a7baf5426b515565855408
Abatarus <giss1302@yandex.ru>
2025-01-18 17:34:45 +0300

    feat: keymap file

---
+++ lua/core/plaguin_keymap.lua
@@ -0,0 +1,34 @@
+keymap = {}
+
+keymap.find_files = "<leader>ff"
+keymap.live_grep = "<leader>fg"
+keymap.find_buffers = "<leader>fb"
+keymap.find_help = "<leader>fh"
+
+keymap.view_git = "<leader>vg"
+keymap.view_tree = "<leader>vt"
+keymap.view_workspaces = "<leader>vw"
+keymap.view_debug = "<leader>vd"
+
+keymap.go_to_references = "<leader>gr"
+keymap.go_to_definition = "<leader>gd"
+keymap.go_to_type_definition = "<leader>gD"
+keymap.go_to_implementation = "<leader>gi"
+keymap.lsp_code_action = "<leader>la"
+keymap.lsp_format_files = "<leader>lf"
+keymap.lsp_rename = "<leader>lr"
+keymap.lsp_signature_help = "<C-k>"
+keymap.lsp_hower = "K"
+
+keymap.view_global_troubles = "<leader>qq"
+keymap.view_buffer_troubles = "<leader>qQ"
+
+keymap.debug_continue = "<F5>"
+keymap.debug_step_over = "<F10>"
+keymap.debug_step_into = "<F11>"
+keymap.debug_step_out = "<F12>"
+keymap.debug_toggle_breakpoint = "<leader>b"
+keymap.debug_set_condotional_breakpoint = "<leader>B"
+keymap.debug_set_log_point = "<leader>dp"
+keymap.debug_repl = "<leader>dr"
+keymap.debug_run_last = "<leader>dl"
