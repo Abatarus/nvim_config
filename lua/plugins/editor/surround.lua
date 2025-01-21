571451094199d6ac2198b7fe2bee009929c83bdb
Abatarus <giss1302@yandex.ru>
2025-01-21 17:24:11 +0300

    feat: keymap for surround

--- lua/plugins/editor/surround.lua
+++ lua/plugins/editor/surround.lua
@@ -4,13 +4,13 @@ return {
     lazy = false,
     opts = {
         mappings = {
-            add = "gza", -- Add surrounding in Normal and Visual modes
-            delete = "gzd", -- Delete surrounding
-            find = "gzf", -- Find surrounding (to the right)
-            find_left = "gzF", -- Find surrounding (to the left)
-            highlight = "gzh", -- Highlight surrounding
-            replace = "gzr", -- Replace surrounding
-            update_n_lines = "gzn", -- Update `n_lines`
+            add = keymap.surrounding_add, -- Add surrounding in Normal and Visual modes
+            delete = keymap.surrounding_delete, -- Delete surrounding
+            find = keymap.surrounding_find, -- Find surrounding (to the right)
+            find_left = keymap.surrounding_find_left, -- Find surrounding (to the left)
+            highlight = keymap.surrounding_highlight, -- Highlight surrounding
+            replace = keymap.surrounding_replace, -- Replace surrounding
+            update_n_lines = keymap.surrounding_update_n_lines, -- Update `n_lines`
         },
     },
 }
