d5542793db05635d38c6441d411d3aec1d0384f1
Abatarus <giss1302@yandex.ru>
2025-01-21 17:09:31 +0300

    feat: surround

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -25,6 +25,8 @@ vim.opt.termguicolors = true
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+vim.o.timeoutlen = 1000
+
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", fg = "none"})
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg = "none"})
 vim.diagnostic.config({
