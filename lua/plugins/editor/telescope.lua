836a9e8471e231780a2aaef9f5f3db2f281be019
Abatarus <giss1302@yandex.ru>
2025-01-21 18:09:29 +0300

    feat: media draw

--- lua/plugins/editor/telescope.lua
+++ lua/plugins/editor/telescope.lua
@@ -24,7 +24,6 @@ return {
                 "%.mat$",
                 "%.FBX$",
                 "%.jpg",
-                "%.png",
                 "%.bmp",
                 "Packages",
                 "PackageChache",
@@ -40,12 +39,20 @@ return {
                 keep_insert = true,
                 path_hl = "String",
             },
+            media_files = {
+                -- filetypes whitelist
+                -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+                filetypes = {"png", "webp", "jpg", "jpeg"},
+                -- find command (defaults to `fd`)
+                find_cmd = "fd",
+                preview_cmd = "chafa",
+            },
         },
     },
-    keys = {
-        { keymap.find_files, function() require("telescope.builtin").find_files() end, desc = "Telescope find files" },
-        { keymap.live_grep, function() require("telescope.builtin").live_grep() end, desc = "Telescope live grep" },
-        { keymap.find_buffers, function() require("telescope.builtin").buffers() end, desc = "Telescope buffers" },
-        { keymap.find_help, function() require("telescope.builtin").help_tags() end, desc = "Telescope help tags" },
-    },
+keys = {
+    { keymap.find_files, function() require("telescope.builtin").find_files() end, desc = "Telescope find files" },
+    { keymap.live_grep, function() require("telescope.builtin").live_grep() end, desc = "Telescope live grep" },
+    { keymap.find_buffers, function() require("telescope.builtin").buffers() end, desc = "Telescope buffers" },
+    { keymap.find_help, function() require("telescope.builtin").help_tags() end, desc = "Telescope help tags" },
+},
 }
