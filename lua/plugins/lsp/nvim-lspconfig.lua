fd0b1aefcf4aa14379a5d9b496ae70fffc2919d3
Abatarus <giss1302@yandex.ru>
2025-01-22 14:58:38 +0300

    feat: avalonia lsp connection

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -22,20 +22,32 @@ return {
         local avalonia_lsp_bin = os.getenv("USERPROFILE") ..
             "/.vscode/extensions/avaloniateam.vscode-avalonia-0.0.32/avaloniaServer/AvaloniaLanguageServer.dll"
 
+        local slnparser_bin = os.getenv("USERPROFILE") ..
+            "/.vscode/extensions/avaloniateam.vscode-avalonia-0.0.32/solutionParserTool/SolutionParser.dll"
         configs.avalonia = {
             default_config = {
                 cmd = { "dotnet", avalonia_lsp_bin },
                 filetypes = { "xaml", "axaml" },
                 capatibilities = capatibilities,
                 root_dir = function(fname)
-                    return lspconfig.util.root_pattern("*.csproj", "*.sln")(fname) or vim.fn.getcwd()
+                    return lspconfig.util.root_pattern("*.sln")(fname) or vim.fn.getcwd()
                 end,
                 settings = {},
-            }
+            },
+            on_new_config = function(_, new_root_dir)
+                print(new_root_dir)
+                local sln_file = nil
+                local sln_files = require("plenary.scandir").scan_dir({ new_root_dir }, { search_pattern = "%.sln$", depth = 1 })
+                if sln_files then
+                    sln_file = sln_files[1]
+                end
+                print(sln_file)
+                vim.fn.system("dotnet " .. slnparser_bin .. " " .. sln_file)
+            end,
         }
         lspconfig.avalonia.setup({
             capatibilities = capatibilities,
-            filetypes = { "xaml", "axaml", "xml" }, -- Укажите оба расширения
+            filetypes = { "xaml", "axaml", "xml" },
         })
         lspconfig.rust_analyzer.setup({
         })
